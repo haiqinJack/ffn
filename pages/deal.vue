fa6c2c2e00ff539926f04c9c5fe2ae703b66b49e
haiqinJack <1198617472@qq.com>
2018-05-27 12:53:25 +0800

    2

--- pages/deal.vue
+++ pages/deal.vue
@@ -204,26 +204,26 @@ export default {
         Toast('请选择订单联系人')
         return 
       }
-      // window.wx.chooseWXPay({
-      //   timestamp: 0, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-      //   nonceStr: '', // 支付签名随机串，不长于 32 位
-      //   package: '', // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
-      //   signType: '', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-      //   paySign: '', // 支付签名
-      //   success: (res) => {
-      //   // 支付成功后的回调函数
-      //     try {
-      //       window.WeixinJSBridge.log(res.err_msg)
-      //     } catch(e) {
-      //       console.error(e)
-      //     }
-      //   }        
-      // })
-      // this.loading = true
-      console.log(total, message, contact, products, 'deal')
       const res = await this.$store.dispatch('createOrder', {total, message, contact, products})
+      const data = res.data.data
 
-      alert(res.data.data.package,'res =======')
+      window.wx.chooseWXPay({
+        timestamp: data.timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+        nonceStr: data.nonceStr, // 支付签名随机串，不长于 32 位
+        package: data.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
+        signType: data.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+        paySign: data.paySign, // 支付签名
+        success: (res) => {
+        // 支付成功后的回调函数
+          try {
+            window.WeixinJSBridge.log(res.err_msg)
+          } catch(e) {
+            console.error(e)
+          }
+        }        
+      })
+      // this.loading = true
+
       WeixinJSBridge.invoke('getBrandWCPayRequest', res.data.data, function(res){
         if(res.err_msg == "get_brand_wcpay_request:ok"){
           alert("支付成功");
