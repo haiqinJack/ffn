3d037dcbc8aa4aaa730f321804b584118f124ae8
haiqinJack <1198617472@qq.com>
2018-05-29 21:03:22 +0800

    2

--- server/controllers/wechat.js
+++ server/controllers/wechat.js
@@ -63,7 +63,7 @@ export async function oauth(ctx, next) {
 }
 
 export async function pay(ctx, next) {
-	let ip = ctx.req.ip_info.clientIp
+	let ip = ctx.ip_info.clientIp
 	ip = ip.replace('::ffff:', '')
 	const { unionid, total, message, contact, products } = ctx.request.body
 	let order = {
