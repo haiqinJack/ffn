46c0a5ded487a3c60390e0cb14f3926950aba8dd
haiqinJack <1198617472@qq.com>
2018-05-31 02:01:04 +0800

    2

--- pages/deal.vue
+++ pages/deal.vue
@@ -177,9 +177,7 @@ export default {
       let obj = {}
       window.wx.openAddress({
         success: function (res) {
-      setTimeout(function() {
-        
-      }, 500)
+  alert('!!')
           obj.name = res.userName// 收货人姓名
           obj.tel = res.telNumber // 收货人手机号码
           obj.postal_code = res.postalCode // 邮编
@@ -191,10 +189,13 @@ export default {
           
         }
       })
-      setTimeout(function() {
-      this.cardType1 = 'edit'
+      // setTimeout(function() {
+      // this.cardType1 = 'edit'
+      // this.currentContact1 = obj
+      // }, 500)
+      alert('???')
+            this.cardType1 = 'edit'
       this.currentContact1 = obj
-      }, 500)
     },
     async payHandle() {
       const total = this.total
