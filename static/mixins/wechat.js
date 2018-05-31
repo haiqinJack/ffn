5df2ed61423feb4422a19f10b8001b01625e72fe
haiqinJack <1198617472@qq.com>
2018-05-31 15:39:09 +0800

    2

--- static/mixins/wechat.js
+++ static/mixins/wechat.js
@@ -1,6 +1,7 @@
 export default {
 	methods: {
 		async wechatInit(url) {
+			url = 'https://www.fafuna.cn'
 			const res = await this.$store.dispatch('getWechatSignture', url)
 			const { data, success } = res.data
 
