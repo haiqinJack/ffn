68f77e9e6a3f7630c73a377aa12d574d8e4ad35a
haiqinJack <1198617472@qq.com>
2018-06-01 21:27:16 +0800

    2

--- server/api/order.js
+++ server/api/order.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose'
 const Order = mongoose.model('Order')
 
 /**
- * 保存商品
- * @param { Object } order 商品对象
+ * 保存订单
+ * @param { Object } order 订单对象
  */
 export async function createOrder(order) {
 	order = new Order(order)
@@ -14,13 +14,18 @@ export async function createOrder(order) {
 	return order
 }
 
-
-export async function fetchOrderByUnionid(unionid, status) {
+/**
+ * 查询订单
+ * @params { String } unionid 用户标示
+ * @params { Number } status 2-待发货, 3-已发货, 5-已完成, 8-维权中
+ * @params { Boolean } success 是否已经付款
+ */
+export async function fetchOrderByUnionid(unionid, status, success=true) {
 	let data = null
 	if(status == 0) {
-		data  = await Order.find({unionid}, 'out_trade_no goods status').sort('-_id').exec()
+		data  = await Order.find({unionid, success}, 'out_trade_no goods status').sort('-_id').exec()
 	}else {
-		data = await Order.find({unionid, status}).sort('-_id').exec()
+		data = await Order.find({unionid, status, success}).sort('-_id').exec()
 	}
 	return data
 }
