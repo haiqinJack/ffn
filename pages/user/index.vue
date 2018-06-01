022bd5852814c8203a6d9e6eb30f2fbe89aacbaa
haiqinJack <1198617472@qq.com>
2018-06-01 11:45:07 +0800

    2

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -3,25 +3,33 @@
     <img class="user-poster" src="https://img.yzcdn.cn/public_files/2017/10/23/8690bb321356070e0b8c4404d087f8fd.png">
     <van-row class="user-links">
       <van-col span="6">
-        <van-icon name="pending-payment"  @click="aa"/>
-        待付款
+        <nuxt-link to="/user/order">
+          <van-icon name="pending-payment" />
+          待付款
+        </nuxt-link>
       </van-col>
       <van-col span="6">
-        <van-icon name="pending-deliver" />
-        待发货
+        <nuxt-link to="/user/order">
+          <van-icon name="pending-deliver" />
+          待发货
+        </nuxt-link>
       </van-col>
       <van-col span="6">
-        <van-icon name="logistics" />
-        已发货
+        <nuxt-link to="/user/order">
+          <van-icon name="logistics" />
+          已发货
+        </nuxt-link>
       </van-col>
       <van-col span="6">
-        <van-icon name="completed" />
-        已完成
+        <nuxt-link to="/user/order">
+          <van-icon name="completed" />
+          已完成
+        </nuxt-link>
       </van-col>
     </van-row>
 
     <van-cell-group class="user-group">
-      <nuxt-link to="/order?status=0">
+      <nuxt-link to="/user/order">
         <van-cell icon="records" title="全部订单" is-link />
       </nuxt-link>
     </van-cell-group>
@@ -33,16 +41,16 @@
     </van-cell-group>
 
     <van-cell-group>
-      <van-cell icon="exchange" title="我的积分" is-link />
-      <van-cell icon="gold-coin" title="我的优惠券" is-link />
-      <van-cell icon="gift" title="我收到的礼物" is-link />
+      <van-cell icon="exchange" title="我的积分" is-link @click="t"/>
+      <van-cell icon="gold-coin" title="我的优惠券" is-link @click="t"/>
+      <van-cell icon="gift" title="我收到的礼物" is-link @click="t"/>
     </van-cell-group>
     <FooterAction />
   </div>
 </template>
 
 <script>
-import { Row, Col, Icon, Cell, CellGroup } from 'vant';
+import { Row, Col, Icon, Cell, CellGroup, Toast } from 'vant';
 import FooterAction from '~/components/footer-action.vue'
 
 export default {
@@ -52,8 +60,8 @@ export default {
     }
   },
   methods: {
-    aa() {
-      alert('??')
+    t() {
+      Toast('升级维护中～')
     }
   },
   components: {
@@ -62,6 +70,7 @@ export default {
     [Icon.name]: Icon,
     [Cell.name]: Cell,
     [CellGroup.name]: CellGroup,
+    [Toast.name]: Toast,
     FooterAction
   }
 };
