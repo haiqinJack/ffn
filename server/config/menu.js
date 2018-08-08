fdfba0880652bb432198601a5bbcdcea19a41be5
haiqinJack <1198617472@qq.com>
2018-08-08 17:06:58 +0800

    update menu.js

--- server/config/menu.js
+++ server/config/menu.js
@@ -3,9 +3,11 @@ import config from './index'
 export default {
 	button: [
 		{
-			type: 'view',
+			type: 'miniprogram',
 			name: '商城首页',
-			url: config.SITE_ROOT_URL + '/allgoods'
+			url: config.SITE_ROOT_URL + '/allgoods',
+			appid: config.wechatMini.appID,
+			pagepath: '/pages/index'
 		}
 	]
 }
